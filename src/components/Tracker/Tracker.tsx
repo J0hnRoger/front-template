2409f7570eb230ed68c879f2dace855139b13f3c
= <=>
2018-01-28 22:00:56 +0100

    Ajout card

---
+++ src/components/Tracker/Tracker.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Tracker extends React.Component {
+  render() {
+   return (
+    <div className="card">
+      <h4 className="placehold__title">Tracker</h4>
+    </div>
+   );
+ }
+}
+
+export default Tracker;
--- src/pages/public/HomePage.tsx
+++ src/pages/public/HomePage.tsx
@@ -4,12 +4,17 @@ import { RouteComponentProps } from 'react-router-dom';
 import Box from 'grommet/components/Box';
 import TopContainer from '../shared/TopContainer';
 
+import Tracker from '../../components/Tracker';
+
 export default class HomePage extends React.Component<RouteComponentProps<{}>, {}> {
     public render() {
         return  <Box>
             <TopContainer>
               <h4>Tracker</h4>
             </TopContainer>
+            <Box pad="large">
+               <Tracker /> 
+            </Box>
         </Box>;
     }
 }
