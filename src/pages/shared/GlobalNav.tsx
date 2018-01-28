c6aa2953a979b809c2b4e71a6b75fedbc281468f
= <=>
2018-01-28 16:55:16 +0100

    Ajout navigation responsive

--- src/pages/shared/GlobalNav.tsx
+++ src/pages/shared/GlobalNav.tsx
@@ -1,20 +1,28 @@
 import * as React from 'react';
-
+import Box from 'grommet/components/Box';
+import Menu from 'grommet/components/Menu';
 import { NavLink } from 'react-router-dom';
 
 class GlobalNav extends React.Component<{}, {}> {
     public render() {
-        return <nav className="sidebar col-xs-12 col-sm-4 col-lg-3 col-xl-2 bg-faded sidebar-style-1">
-            <a href="#menu-toggle" className="btn btn-default" id="menu-toggle">
-                <em className="fa fa-bars" /></a>
-            <ul className="nav nav-pills flex-column sidebar-nav">
-                <NavLink exact to={'/home'} className="nav-link">
-                  <li className="nav-item">
-                  Home
-                  </li>
+        return (
+            <Box flex="grow"
+            justify="start"
+            >
+                <Menu primary={true}
+                    fill={true}
+                >
+                <NavLink to="/">
+                    Home
                 </NavLink>
-            </ul>
-        </nav>;
+                <NavLink to="/details">
+                    Details
+                </NavLink>
+                <NavLink to="/account">
+                    Account
+                </NavLink>
+                </Menu>
+            </Box>);
     }
 }
 
--- src/pages/shared/Layout.tsx
+++ src/pages/shared/Layout.tsx
@@ -1,32 +1,73 @@
 import * as React from 'react';
-
 import App from 'grommet/components/App';
+import Article from 'grommet/components/Article';
+import Header from 'grommet/components/Header';
 import Section from 'grommet/components/Section';
 import Split from 'grommet/components/Split';
 import Sidebar from 'grommet/components/Sidebar';
-import Box from 'grommet/components/Box';
+import Title from 'grommet/components/Title';
+import Animate from 'grommet/components/Animate';
+import Button from 'grommet/components/Button';
+
+import MenuIcon from 'grommet/components/icons/base/Menu';
+
+import 'grommet-css';
 
 /** Components **/
 import GlobalNav from '../shared/GlobalNav';
-import Header from '../shared/Header';
+// import Header from '../shared/Header';
 
-export class Layout extends React.Component {
+interface Props {
+}
+
+interface State {
+    showNav: boolean;
+}
+
+export class Layout extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            showNav: false,
+        };
+    }
+
+    handleShowNav = (evt) => {
+        this.setState({ showNav: !this.state.showNav });
+    }
+
     public render() {
         return <App>
-          <Header />
-          <Split>
-            <Sidebar>
-              <Box 
-                flex="grow"
-                justify="start"
-              >
-                <GlobalNav />
-              </Box>
-            </Sidebar>
-            <Section>
-              {this.props.children}
-            </Section>
-          </Split>
-    </App>;
+             <Header pad="medium">
+                <Button
+                icon={<MenuIcon />}
+                plain={true}
+                onClick={this.handleShowNav} />
+                <Title>Front-Starter</Title>   
+            </Header>
+            <Split priority={this.state.showNav ? 'left' : 'right'}>
+                <Animate enter={{'animation': 'slide-down', 'duration': 1000}} 
+                        leave={{'animation': 'slide-up', 'duration': 1000}} 
+                    >
+                    <Sidebar
+                        colorIndex="neutral-1"
+                            size="small">
+                        <Header 
+                            pad="medium"
+                            justify="between">
+                            <Title>
+                            Training App
+                            </Title>
+                        </Header>
+                        <GlobalNav />
+                    </Sidebar>
+                </Animate>
+                <Article scrollStep={false}>
+                    <Section>
+                    {this.props.children}
+                    </Section>
+                </Article>
+            </Split>
+        </App>;
   }
 }
