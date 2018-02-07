63200f7f853c391cc2edc8098cc96b70f6caf715
= <=>
2018-02-07 22:42:38 +0100

    Adding 'raf/polyfill' for supporting requestAnimationFrame

--- config/jest/jest.setup.ts
+++ config/jest/jest.setup.ts
@@ -1,4 +1,6 @@
+import 'raf/polyfill';
+
 import { configure } from 'enzyme';
 import * as ReactSixteenAdapter from 'enzyme-adapter-react-16';
 
-configure({ adapter: new ReactSixteenAdapter() });
+configure({ adapter: new ReactSixteenAdapter(), disableLifecycleMethods: true });
--- src/components/Tracker/__tests__/Tracker.test.tsx
+++ src/components/Tracker/__tests__/Tracker.test.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { } from 'jest';
 import { shallow } from 'enzyme';
 
 import Tracker from '../';
