362a41f98657bf38fcca77561abf54d7aa993be8
= <=>
2018-06-12 19:36:43 +0200

    Ajout cross-env + config CSS

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,22 @@ const path = require('path');
 const HtmlPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+const devMode = process.env.NODE_ENV !== 'production';
+
+console.log(devMode);
+
 module.exports = {
+    watch: devMode,
     module: {
-        rules: [
-            {
-            test: /\.css$/,
+        rules: [{
+            test: /\.(sa|sc|c)ss$/,
             use: [
-                MiniCssExtractPlugin.loader,
-                "css-loader"
-            ]
-            }
+                devMode
+                ? 'style-loader'
+                : MiniCssExtractPlugin.loader,
+                "css-loader",
+                "sass-loader"
+            ]}
         ]
     },
     plugins: [
