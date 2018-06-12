7c08c643d56f9b0fc2c2985022322e8cdf9e63cf
= <=>
2018-06-12 21:03:46 +0200

    Adding SASS support + caching hash

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import utils from './utils';
-import './style.css';
+import './style.scss';
 
 utils.log("front");
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const HtmlPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 const devMode = process.env.NODE_ENV !== 'production';
 
-console.log(devMode);
-
-module.exports = {
+const config = {
     watch: devMode,
+    output: {
+        filename: devMode ? '[name].js' : '[name].[chunkhash].js'
+    },
     module: {
         rules: [{
             test: /\.(sa|sc|c)ss$/,
@@ -22,6 +24,16 @@ module.exports = {
     },
     plugins: [
         new HtmlPlugin(),
-        new MiniCssExtractPlugin(),
+        new MiniCssExtractPlugin({
+            filename: devMode ? '[name].css' : '[name].[chunkhash].css',
+            chunkFilename: devMode ? '[id].css' : '[id].[chunkhash].css',
+        }),
+        new CleanWebpackPlugin(['dist'], {
+            root: path.resolve('./'),
+            verbose: true,
+            dry: false,
+        }),
     ]
 };
+
+module.exports = config
